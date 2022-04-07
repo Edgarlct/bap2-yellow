af58b70f8a94a7d007152aec4bb58ff3b76c120b
Edgarlct <90525103+Edgarlct@users.noreply.github.com>
2022-04-07 09:25:36 +0200

    :bento: add fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+    public function load(ObjectManager $manager): void
+    {
+         $user = new User();
+         $user->setEmail('a@a.fr');
+         $user->setRoles(['ROLE_USER']);
+         $user->setFirstName('a');
+         $user->setLastName('a');
+         $user->setAge('21');
+         $user->setIsSub(true);
+         $password = $this->hasher->hashPassword($user, 'a');
+
+         $user->setPassword($password);
+         $manager->persist($user);
+
+         $manager->flush();
+    }
+}
