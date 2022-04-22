e763766922000e88d4829418e7cae5550042c99d
Edgarlct <edgar.lecomte@icloud.com>
2022-04-22 02:46:27 +0200

    :construction: work on newsletter form

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,21 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
 use App\Repository\EquipeRepository;
 use App\Repository\OfferRepository;
 use App\Repository\SocialRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app')]
-    public function index(OfferRepository $offerRepository, EquipeRepository $equipeRepository,): Response
+    public function index(OfferRepository $offerRepository, EquipeRepository $equipeRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $newsletter = new Newsletter();
+        $newsletterForm = $this->createForm(NewsletterType::class, $newsletter);
+
+        $newsletterForm->handleRequest($request);
+        if ($newsletterForm->isSubmitted() && $newsletterForm->isValid()){
+            $newsletter->setEmail($newsletterForm->get("email")->getData());
+            $newsletter->setIsSub(true);
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
+        }
         return $this->render('app/index.html.twig', [
             'offers' => $offerRepository->findAll(),
             'teamMembers' => $equipeRepository->findAll(),
+            'newsletterForm' => $newsletterForm->createView(),
         ]);
     }
 
