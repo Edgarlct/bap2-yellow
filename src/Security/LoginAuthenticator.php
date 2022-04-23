6ec5ea00b3747acd475022f9ae644a0338ddd879
Edgarlct <edgar.lecomte@icloud.com>
2022-04-23 20:00:38 +0200

    :bug: redirect to homepage

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -45,12 +45,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if (in_array('ROLE_ADMIN', $token->getUser()->getRoles())) {
-            return new RedirectResponse($this->urlGenerator->generate('admin'));
-        }
-        else{
-            return new RedirectResponse($this->urlGenerator->generate('app'));
-        }
+        return new RedirectResponse($this->urlGenerator->generate('app'));
     }
 
     protected function getLoginUrl(Request $request): string
